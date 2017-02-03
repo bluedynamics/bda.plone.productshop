5a8f461bd00a212c7106d7e8fb70242bc4c7d4f9
Robert Niederreiter <office@squarewave.at>
2017-02-03 11:56:59 +0100

    make buildout work again

--- bootstrap-4.3.x.sh
+++ bootstrap-4.3.x.sh
@@ -2,7 +2,7 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./local ./bin
-ln -fs plone-5.0.cfg buildout.cfg
+ln -fs plone-4.3.x.cfg buildout.cfg
 virtualenv --clear .
 ./bin/pip install --upgrade pip setuptools zc.buildout
-./bin/buildout
+./bin/buildout 
