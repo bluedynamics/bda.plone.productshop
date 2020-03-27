84670ed9933c7f6d141294fdd9e838f18ec344d7
Jens W. Klein <jk@kleinundpartner.at>
2020-03-27 10:34:52 +0100

    cleanup after merge

--- bootstrap-5.1.x.sh
+++ bootstrap-5.1.x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-# see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./local ./bin
 ln -fs plone-5.1.x.cfg buildout.cfg
-virtualenv --clear .
-./bin/pip install --upgrade pip setuptools zc.buildout
+python3 -mvenv .
+./bin/pip -U pip
+./bin/pip install -r requirements.cfg 
 ./bin/buildout
+
