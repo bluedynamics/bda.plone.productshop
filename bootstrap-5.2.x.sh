afa28307cf4bf986ae6b67f2076ac2c4d1f6cf87
Jens W. Klein <jk@kleinundpartner.at>
2020-04-29 20:16:49 +0200

    fix bootstrap and buildout

--- bootstrap-5.2.x.sh
+++ bootstrap-5.2.x.sh
@@ -3,6 +3,6 @@ rm -r ./lib ./include ./local ./bin
 ln -fs plone-5.2.x.cfg buildout.cfg
 python3 -mvenv .
 ./bin/pip -U pip
-./bin/pip install -r requirements.cfg
+./bin/pip install -r requirements.txt
 ./bin/buildout
 
